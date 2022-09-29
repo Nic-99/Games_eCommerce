406daac6a3b6558dd310d6053e5fc985282851fb
Nico <cuenta.foros.prog@gmail.com>
2022-09-29 10:25:24 -0300

    Creacion del modelo y controlador del usuario + updates en el index + chequeo de autorizacion con el middleware

--- index.js
+++ index.js
@@ -5,70 +5,160 @@ const http = require("http").createServer(app);
 const cors = require("cors");
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-//const { adduser, getUser, deleteUser, getUsers} = require("./users");
-
 const uri = process.env.MONGO_URI;
 
-/*
+/******/
+const UsrController = require('./controllers/user');
+const AuthController = require('./controllers/auth');
+const Middleware = require('./middleware/auth-middleware');
+
+
 mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("connected");
-    })
-    .catch((err) => consol.log(err))
-*/
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => console.log(err));
+
 
 app.use(cors());
 app.use(express.json());
 
+// GET - POST - DELETE - PUT - PATCH 
 
 app.get("/", (req, res) => {
-    res.send("Hola estoy funcionando. ");
+  res.send("Hola estoy funcionando.");
+});
+app.post("/",(req,res) => {
+    res.send("Llamada post");
 });
 
-// Obtengo info de los usarios
-app.get("/users", (req,res) => {
+// Get de todos los usuarios
+app.get("/users",/*Middleware.verify,*/async (req,res) =>{
 
-    res.send("Listo los usuarios");
+  let limit = req.query.limit;
+  let offset = req.query.offset;
+
+  try{
+      const results = await UsrController.getAllUsers(limit,offset);
+      res.status(200).json(results);
+
+  }catch(error){
+      res.status(500).send("Error. Intente más tarde.")
+  }
+
 });
 
-// Obtengo info de un usuario especifico
-app.get("/users/:id", (req,res) => {
-    
-    let userId = req.params.id;
-    //console.log(userId);
-    //res.send("Obtengo un usuario");
-    user = {"name":"Pepe", "lastname":"Alegria"};
-    res.json(user);
-});
+// Get Info de un usuario
+app.get("/users/:id",async (req,res) =>{
 
-app.post("/", (req,res) =>{
-    res.send("Llamada Post");
+    let userId =  req.params.id;
+
+    try{
+
+      user = await UsrController.getUser(userId);
+
+      res.status(200).json(user);
+
+    }catch(error){
+      res.status(500).send("Error");
+    }
+
 });
 
 // Creo un nuevo usuario
-app.post("/users", (req,res) => {
-
+app.post("/users",async (req,res) =>{
+    
     let name = req.body.name;
     let lastname = req.body.lastname;
-    console.log(name);
-    res.send("Creo usuario");
+    let email = req.body.email;
+    let isActive = req.body.isActive;
+    let password = req.body.password;
+    try{
+      const result = await UsrController.addUser(name,lastname,email,isActive,password);
+      if(result){
+        res.status(201).send("Usuario creado correctamente"); // 201
+      }else{
+        res.status(409).send("El usuario ya existe"); // 409
+      }  
+    }catch(error){
+      res.status(500).send("Error al crear el usuario."); //500
+    }  
+    
 });
 
-// Modifico un ususario
-app.put("/users/:id", (req,res) => {
+// Modifico un usuario
+app.put("/users/:id",async (req,res) =>{
 
-    const userId = req.params.id;
-    let name = req.body.name;
-    console.log(name);
-    res.send("Edito usuario");
+    const user = { _id: req.params.id, ...req.body };
+    //             {_id: req.params.id, name: req.body.name, lastname, email }
+    try{
+      
+      const result = await UsrController.editUser(user);
+      if(result){
+        res.status(200).json(result);
+      }else{
+        res.status(404).send("El usuario no existe.");
+      }  
+    }catch(error){  
+       res.status(500).send("Error");
+    } 
+
 });
 
-// Elimino usuario
-app.delete("/users/:id", (req,res) => {
-    res.send("Elimino un usuario");
+// Elimino un usuario
+app.delete("/users/:id", async(req,res) =>{
+
+    try{
+
+      const result = await UsrController.deleteUser(req.params.id);
+      if(result){
+        res.status(200).send("Usuario borrado.")
+      }else{
+        res.status(404).send("No se ha podido eliminar el usuario.")
+      }  
+
+    }catch(error){
+      res.status(500).send("Error")
+    }
 });
+
+// Edito roldes del usuario
+app.put("/users/:id/roles",async (req,res) =>{
+    
+    const roles = req.body.roles;
+    try{
+      
+      const result = await UsrController.editRoles(roles,req.params.id);
+      if(result){
+        res.status(200).json(result);
+      }else{
+        res.status(404).send("El usuario no existe.");
+      }  
+    }catch(error){  
+       res.status(500).send("Error");
+    } 
+})
+
+// TODO --> Copia del edit roles para isActive
+
+app.post("/auth/login", async (req,res) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    try{
+      const result = await AuthController.login(email,password);
+      if(result){
+        res.status(200).json(result);
+      }else{
+        res.status(401).send("No puede estar aqui")
+      }
+    }catch(error){
+        res.status(500).send("Error");
+    }  
+})
+
 
 http.listen(PORT, () => {
-    console.log(`Listening to ${PORT}`);
+  console.log(`Listening to ${PORT}`);
 });
